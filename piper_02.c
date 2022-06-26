23c95befa8e6fa8a2d8658828623d3b2921ac185
HeitorNunes275 <88085970+HeitorNunes275@users.noreply.github.com>
2022-06-26 16:46:53 -0300

    Add files via upload

---
+++ piper_02.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void) {
+
+    int fd[2];
+    pid_t id = fork();
+
+    if (pipe(fd)<0) {
+        perror("pipe");
+        return -1;
+    }
+
+    if ((id = fork()) < 0) {
+        perror("fork");
+        exit(1);
+    }   
+
+    if (id > 0) {
+        int somaPares;
+        close(fd[1]);
+        
+
+        int somaImpares;
+        for (int i=0; i < 10; i++) {
+            if (i%2 != 0) {
+                somaImpares += i;
+            }
+        }
+
+        read(fd[0], &somaPares, sizeof(somaPares));
+        printf("A soma dos números de 1 a 10 é %d.\n", somaPares+somaImpares);
+
+    }
+
+    if (id == 0) {
+        
+        close(fd[0]);
+        
+        int somaPares;
+        for (int i=0; i <= 10; i++) {
+            if (i%2 == 0) {
+                somaPares += i;
+            }
+        }
+
+        write(fd[1], &somaPares, sizeof(somaPares));
+
+    }
+
+    
+}
